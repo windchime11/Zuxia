2910ef9e830c2bc5b870387f98a786db32f0cc25
JZ <east54john@gmail.com>
2012-02-05 21:28:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zuxia::Application.config.secret_token = '7ccce593c1f02cd5123c2271f6cb4d9aa8fabbfde9d90ee58d9af583fe87571c7b7f88701aa4211a77abefcb9ef3bdcbcb3f710bb6998684b9bbe1db2c192a4c'
